ca115c8e501d32e75e72d9223b852f54afb249fa
Yoseph won <yosephwondimu26@gmail.com>
2025-10-05 18:05:29 +0300

    Fixing logout functioning

--- announcement/urls.py
+++ announcement/urls.py
@@ -2,11 +2,11 @@ from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
 from . import views
 from .views import AnnouncementListView, AnnouncementCreateView
-
+from django.urls import reverse_lazy
 urlpatterns = [
     path('', views.home_view, name='home'),
     path('login/', LoginView.as_view(template_name='announcement/login.html'), name='login'),
-    path('login/', LogoutView.as_view(template_name='announcement/logout.html'), name='logout'),
+    path('logout/', LogoutView.as_view(next_page=reverse_lazy('home')), name='logout'),
     path('register/', views.user_register_view, name='register'),
     path('profile/<int:pk>', views.profile_view, name='profile'),
 
