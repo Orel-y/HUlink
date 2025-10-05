1622bc15d6db6c7faee8684f5d86b29f2b225906
Yoseph won <yosephwondimu26@gmail.com>
2025-10-05 16:58:04 +0300

    Handled The Announcement Creation and Listing

--- announcement/views.py
+++ announcement/views.py
@@ -3,7 +3,11 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
-
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Announcement
+from .forms import AnnouncementForm
+from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
+from django.urls import reverse_lazy
 
 def user_register_view(request):
     if request.method == 'POST':
@@ -28,3 +32,19 @@ def profile_view(request, pk):
     return render(request, 'announcement/profile.html', {"user_profile": user_profile})
 
 #<li><a href="{%url 'announcements'%}">Announcements &#128226; </a></li>
+
+class AnnouncementListView(ListView):
+    model = Announcement
+    template_name = 'announcement/announcement_list.html'
+    context_object_name = "announcements"
+    ordering = ["-created_at"]
+
+class AnnouncementCreateView(CreateView):
+    model = Announcement
+    template_name = 'announcement/announcement_form.html'
+    form_class = AnnouncementForm
+    success_url = reverse_lazy('announcement_list')
+    
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
