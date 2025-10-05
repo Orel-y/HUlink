a92ecf4e365fe3ac08cb0d6cd2631bab558e3d67
Yoseph won <yosephwondimu26@gmail.com>
2025-10-05 18:28:32 +0300

    Tested Authentication

--- HUlink/settings.py
+++ HUlink/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+from django.urls import reverse_lazy
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -125,4 +125,4 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/announcement/'
-LOGOUT_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = reverse_lazy('login')
--- announcement/urls.py
+++ announcement/urls.py
@@ -6,7 +6,7 @@ from django.urls import reverse_lazy
 urlpatterns = [
     path('', views.home_view, name='home'),
     path('login/', LoginView.as_view(template_name='announcement/login.html'), name='login'),
-    path('logout/', LogoutView.as_view(next_page=reverse_lazy('home')), name='logout'),
+    path('logout/', LogoutView.as_view(template_name='announcement/logout.html'), name='logout'),
     path('register/', views.user_register_view, name='register'),
     path('profile/<int:pk>', views.profile_view, name='profile'),
 
